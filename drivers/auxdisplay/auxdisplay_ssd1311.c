0f7ae077de347e2915a1100523f1eb863ce33a47
Christian Frey <christian.frey@embedded-insights.ch>
2025-01-27 15:20:26 +0100

    drivers/ssd1311: add config for nr. of lines (not tested yet)

--- drivers/auxdisplay/auxdisplay_ssd1311.c
+++ drivers/auxdisplay/auxdisplay_ssd1311.c
@@ -12,6 +12,8 @@ LOG_MODULE_REGISTER(ssd1311, CONFIG_AUXDISPLAY_LOG_LEVEL);
 struct ssd1311_config {
     struct i2c_dt_spec bus;
     int lines;
+    int font_width;
+    bool invert_cursor;
 };
 
 struct ssd1311_data {
@@ -53,6 +55,24 @@ static int ssd1311_display_off(const struct device* dev) {
     return ssd1311_display_ctrl(dev);
 }
 
+static int ssd1311_custom_command(const struct device *dev, struct auxdisplay_custom_data *command) {
+    const struct ssd1311_config* config = dev->config;
+    return i2c_write_dt(&config->bus, command->data, command->len);
+}
+
+static int ssd1311_brightness_set(const struct device* dev, uint8_t brightness) {
+    const struct ssd1311_config* config = dev->config;
+
+    //uint8_t cmd = 0x2a;
+    ssd1311_send_cmd(dev, 0x2a);
+    ssd1311_send_cmd(dev, 0x79);
+    ssd1311_send_cmd(dev, 0x81);
+    ssd1311_send_cmd(dev, brightness);
+
+    ssd1311_send_cmd(dev, 0x78);
+    ssd1311_send_cmd(dev, 0x28);
+}
+
 static int ssd1311_init(const struct device* dev) {
     const struct ssd1311_config* config = dev->config;
 
@@ -62,16 +82,33 @@ static int ssd1311_init(const struct device* dev) {
     // send 0x28 -> clr RE bit
     // send 0x79 -> enable oled cmd set
     // send 0x78 -> disable oled cmd set
+    
+    // extended function set
+    ssd1311_send_cmd(dev, 0x22 | ((config->lines == 2 || config->lines == 4) ? 0x08 : 0x00) /*0x2a*/);
+
     uint8_t cmd = 0x08;
-    ssd1311_send_cmd(dev, 0x2a);
 
     if(config->lines == 3 || config->lines == 4) {
         cmd |= 0x01;
     }
-    // if invert_cursor |= 0x02
-    // if 6dot-font |= 0x04
-    ssd1311_send_cmd(dev, cmd);
 
+    // if 6dot-font |= 0x04
+    if(config->font_width == 6) {
+        cmd |= 0x04;
+    }
+
+    // if invert_cursor |= 0x02
+    if(config->invert_cursor) {
+        cmd |= 0x02;
+    }
+    
+    ssd1311_send_cmd(dev, cmd);
+/*
+    cmd = 0x10; // 1-1-2
+    cmd = 0x14; // 1-2-1
+    cmd = 0x18; // 2-2
+    cmd = 0x1C; // 2-1-1
+*/
     ssd1311_send_cmd(dev, 0x28);
     return -ENOTSUP;
 }
@@ -79,11 +116,17 @@ static int ssd1311_init(const struct device* dev) {
 static DEVICE_API(auxdisplay, ssd1311_api) = {
     .display_on = ssd1311_display_on,
     .display_off = ssd1311_display_off,
+    .brightness_get = NULL,
+    .brightness_set = ssd1311_brightness_set,
+    .custom_command = ssd1311_custom_command,
 };
 
 #define SSD1311_INIT(inst)          \
     static const struct ssd1311_config ssd1311_config_##inst = {    \
         .bus = I2C_DT_SPEC_INST_GET(inst),                          \
+        .lines = DT_INST_PROP(inst, rows),                          \
+        .font_width = DT_INST_PROP(inst, font_width),               \
+        .invert_cursor = DT_INST_PROP(inst, invert_cursor),         \
     };                                                              \
     static struct ssd1311_data ssd1311_data_##inst = {0u};          \
                                                                     \
